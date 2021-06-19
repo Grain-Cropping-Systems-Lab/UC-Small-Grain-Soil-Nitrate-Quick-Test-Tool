7aabb971545b350888ea4e18202ddc07beea57ad
tsnelsen <tsnelsen@ucdavis.edu>
2021-06-18 17:11:12 -0700

    update with bulk density model and wording changes

--- qtconverter/server.R
+++ qtconverter/server.R
@@ -12,7 +12,7 @@ server <- function(input, output, session) {
 	# values are reactive so they recalculate whenever there is new inputs
 	soil_data <- reactive({
 		if(!is.null(map_outputs$lon)){
-		dbquery <- DBI::dbGetQuery(con, paste("SELECT muname, omr, dbvndr FROM grain.ssurgo WHERE ST_Contains(geom, ST_GeomFromText('POINT(", req(map_outputs$lon), req(map_outputs$lat), ")',4326));"))
+		dbquery <- DBI::dbGetQuery(con, paste("SELECT muname, omr, dbthrd FROM grain.ssurgo WHERE ST_Contains(geom, ST_GeomFromText('POINT(", req(map_outputs$lon), req(map_outputs$lat), ")',4326));"))
 		dbquery[,2] <- ifelse(!is.na(dbquery[,2]), as.numeric(dbquery[,2]), 1.01010101010101)
 		return(dbquery)
 		}
@@ -24,26 +24,31 @@ server <- function(input, output, session) {
 	observe({
 		
 		if(!is.null(map_outputs$lon)){
-
-		updateNoUiSliderInput(session, "bulk_density", value = round(as.numeric(soil_data()[,3]), 2))
-
+		
+		soilqt <- qtconversion(padvalue = input$padvalue, bd = input$bulk_density, extractant = input$cacl2)
+		
 		output$htmlout <- renderText({
 
 			if (soil_data()[, 2] != 1.01010101010101 & !is.na(map_outputs$lat)){
-				soilqt <- qtconversion(padvalue = input$padvalue, bd = input$bulk_density, extractant = input$cacl2)
+				if(soil_data()[,3] > 1.6 | soil_data()[,3] < 0.5){
+					paste("<b><font color='red'>The bulk density at this location is outside of our observed range.</b></font>")
+				} else {
+				#soilqt <- qtconversion(padvalue = input$padvalue, bd = input$bulk_density, extractant = input$cacl2)
 				paste("For the given field in <b>",
 							DBI::dbGetQuery(con, paste("SELECT namelsad FROM grain.ca_counties WHERE ST_Contains(geom, ST_GeomFromText('POINT(", map_outputs$lon, map_outputs$lat, ")',4326));"))$namelsad, "</b> <br>",
 							"that has a soil type of <b>", soil_data()[, 1], "</b> <br>",
 							"which is a <b>", ifelse(as.numeric(soil_data()[, 2]) > 5, "organic soil", "mineral soil"), "</b>",
-							"with an approximate dry bulk density of <b>", input$bulk_density, "g/cm<sup>3</sup> </b> <br>",
+							"with an approximate bulk density of <b>", input$bulk_density, "g/cm<sup>3</sup> </b> <br>",
 							"with a pad value of <b>", input$padvalue, "</b> <br>",
 							"tested with <b>", ifelse(input$cacl2 == "water", "water", "Calcium Chloride"), "</b>",
-							"the lab nitrate-N eqivalent is <b> <font color='red'>", round(soilqt[, 2], 1), "ppm &plusmn;",
-							round(mean(soilqt[, 3] - soilqt[, 2], soilqt[, 2] - soilqt[, 1]), 1), "</b> </font> <br>",
-							"and the fertilizer eqivalent in the soil tested is <b> <font color='red'>",
-							round(soilqt[, 2]*(43560*as.numeric(input$bulk_density)*62.428/1000000), 0),
-							"lb N/ac &plusmn;",
-							round((mean(soilqt[, 3] - soilqt[, 2], soilqt[, 2] - soilqt[, 1]))*(43560*as.numeric(input$bulk_density)*62.428/1000000), 0), "</b> </font>")
+							"the lab ", ifelse(input$nform == "nitraten", "nitrate-N", "nitrate"), 
+							" equivalent is <b> <font color='red'>", input$labvalue, "ppm &plusmn;",
+							round(mean(soilqt[, 3] - input$labvalue, input$labvalue - soilqt[, 1]), 1), "</b> </font> <br>",
+							"and the approximate nitrate-N fertilizer equivalent in the soil tested is <b> <font color='red'>",
+							round(input$labvalue*(43560*as.numeric(input$bulk_density)*62.428/1000000), 0),
+							"N lb/ac &plusmn;",
+							round((mean(soilqt[, 3] - input$labvalue, input$labvalue - soilqt[, 1]))*(43560*as.numeric(input$bulk_density)*62.428/1000000), 0), "</b> </font>")
+				}
 
 			} else {
 				paste("<b><font color='red'>There is no soil data.</b></font>")
@@ -52,12 +57,41 @@ server <- function(input, output, session) {
 
 		})
 		
+		
+		
 		}
 
+	})
+	
+	observe({
+		updateNoUiSliderInput(session, "bulk_density", value = round(as.numeric(soil_data()[,3]), 2))
 	})
 
 	observeEvent(input$reset_bd, {
 		updateNoUiSliderInput(session, "bulk_density", value = round(as.numeric(soil_data()[,3]), 2))
+	})
+	
+	observeEvent(input$padvalue, {
+		soilqt <- qtconversion(padvalue = input$padvalue, bd = input$bulk_density, extractant = input$cacl2)
+		if(round(soilqt[, 2], 1) != input$labvalue){
+		updateNoUiSliderInput(session, "labvalue", value = round(soilqt[, 2], 1))
+		}
+			
+	})
+	
+	observeEvent(input$bulk_density, {
+		soilqt <- qtconversion(padvalue = input$padvalue, bd = input$bulk_density, extractant = input$cacl2)
+		updateNoUiSliderInput(session, "labvalue", value = round(soilqt[, 2], 1))
+	})
+	
+	observeEvent(input$labvalue, {
+		labval <- reverse_qtconversion(labvalue = input$labvalue,
+																	 bd = input$bulk_density,
+																	 extractant = input$cacl2)
+		if(labval != input$padvalue){
+			updateNoUiSliderInput(session, "padvalue",
+														value = labval)
+		}
 	})
 
 }
--- qtconverter/ui.R
+++ qtconverter/ui.R
@@ -12,16 +12,19 @@ ui <- dashboardPage(
 			),
 		tabItem(tabName = "location",
 							fluidRow(
+								column(6,
 								box(title = "Location", solidHeader = TRUE, status = "primary", 
+										width = 12, 
 										p("Click or move the marker to the field where the soil sample was taken. You must choose a field within the agricultural lands of CA (non-shaded region)."),
 										map_mod_ui("map_mod")
 										),
 								box(title = "Quick Test Value", solidHeader = TRUE, status = "primary",
+										width = 12, 
 									shinyWidgets::noUiSliderInput(
 										inputId = "padvalue", 
 										label = "Enter your pad value from the soil nitrate quick test (ppm). Results are based on top 0-12 in of soil.",
 										color = "#005fae",
-										value = 10,
+										value = NA,
 										min = 0,
 										max = 50,
 										tooltips = TRUE,
@@ -36,25 +39,43 @@ ui <- dashboardPage(
 								radioButtons("cacl2", label = "", 
 														 choices = list("Calcium Chloride used in shaking solution" = "cacl2", 
 														 							 "Water-only used in shaking solution" = "water"), selected = "cacl2")
+								)
 								),
-								box(
-										htmlOutput("htmlout"), # use htmlOutput so can format the text to be bold, colored, etc.
-										bsTooltip("htmlout", title = "The error provided shows the 95% confidence interval for the values shown.", placement = "top")
-									),
-								box(
+								column(6, 
+								box(title = "Lab Value", solidHeader = TRUE, status = "primary",
+										width = 12, 
+										shinyWidgets::noUiSliderInput(
+											inputId = "labvalue", 
+											label = "Alternatively, if you have a lab value you may select it here. Results are based on nitrate-N (ppm) in the top 0-12 inches of soil (to translate nitrate to nitrate-N multiply by 0.23).",
+											color = "#005fae",
+											value = NA,
+											min = 0,
+											max = 50,
+											tooltips = TRUE,
+											pips = list(
+												mode = "values",
+												values = c(0, 10, 20, 30, 40, 50),
+												format = wNumbFormat(decimals = 0)
+											),
+											step = 0.1,
+											format = wNumbFormat(decimals = 1)
+										)
+								),
+								box(width = 12, 
+										title = "Bulk Density", solidHeader = TRUE, status = "primary",
 									shinyWidgets::noUiSliderInput(
 										inputId = "bulk_density", 
-										label = HTML("The results are based on the oven dry bulk density (g/cm<sup>3</sup>) from the SSURGO
+										label = HTML("The results are based on the in situ bulk density (g/cm<sup>3</sup>) from the SSURGO
 database. Values used and presented here are a weighted average of the component types present in
-the top 0-12 in of soil. Adjust the site-specific bulk density if needed."),
+the top 0-12 in of soil. SSURGO-estimated bulk density may not accurately represent the bulk density at a site. For better accuracy, update the SSURGO estimate with a recently-measured bulk density value."),
 										color = "#005fae",
 										value = 1.25,
 										min = 0.5,
-										max = 2.0,
+										max = 1.6,
 										tooltips = TRUE,
 										pips = list(
 											mode = "values",
-											values = c(0.5, 1, 1.5, 2),
+											values = c(0.5, 1, 1.5),
 											format = wNumbFormat(decimals = 1)
 										),
 										step = 0.01,
@@ -63,12 +84,18 @@ the top 0-12 in of soil. Adjust the site-specific bulk density if needed."),
 									br(),
 									bsButton("reset_bd", label = "Reset", icon("undo-alt"), block = TRUE, size = "lg")
 								),
-								column(6, offset = 6,
+								box(title = "Soil Results", solidHeader = TRUE, status = "primary",
+										width = 12, 
+										htmlOutput("htmlout"), # use htmlOutput so can format the text to be bold, colored, etc.
+										bsTooltip("htmlout", title = "The error provided shows the 95% confidence interval for the values shown.", placement = "top")
+								),
+								#column(6, offset = 6,
 									"For more information on how to perform the soil nitrate quick test see",
 									a("http://smallgrains.ucanr.edu/Nutrient_Management/snqt/", href="http://smallgrains.ucanr.edu/Nutrient_Management/snqt/")
-								)
+							#	)
 								
 							)
 	)
 	)
+)
 )
